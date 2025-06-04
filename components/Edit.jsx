9ad7a63159381c8ba71d7d7d9d75e2b31b583209
na-kyoung <skrud310@naver.com>
2025-06-04 09:51:52 +0900

    Create Menu Bar

--- components/Edit.jsx
+++ components/Edit.jsx
@@ -2,6 +2,7 @@ import useImageStore from "@/lib/store/useImageStore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import classes from "@/styles/Edit.module.css";
+import EditMenu from "./EditMenu";
 
 export default function Edit(){
   const router = useRouter();
@@ -19,7 +20,7 @@ export default function Edit(){
       return () => URL.revokeObjectURL(url);
     } else {
       alert('선택한 사진이 없습니다.');
-      // router.replace('/');
+      router.replace('/');
       // 히스토리 스택 중복 문제 해결하기
       // window.location.replace('/page1'); // 상태 초기화 & 새로고침됨
     }
@@ -27,12 +28,13 @@ export default function Edit(){
 
   return (
     <>
-      <div className={classes.editbox}>
-        <p className={classes.edittitle}>Edit Page</p>
+      <div className={classes.edit_box}>
+        <p className={classes.edit_title}>Edit Page</p>
         {imageUrl ? (
-          <img src={imageUrl} alt="선택된 이미지" className={classes.editimg} />
+          <img src={imageUrl} alt="선택된 이미지" className={classes.edit_img} />
         ) : (<p>사진 로딩중..</p>)}
       </div>
+      <EditMenu />
     </>
   );
 }
