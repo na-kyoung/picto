1d86b1f54c07004acaac04e446180191a4ea3c63
na-kyoung <skrud310@naver.com>
2025-06-05 16:47:39 +0900

    update css

--- components/EditMenu.jsx
+++ components/EditMenu.jsx
@@ -12,6 +12,10 @@ export default function EditMenu(){
     { id: 'beauty', title: '뷰티', icon: <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M620-520q25 0 42.5-17.5T680-580q0-25-17.5-42.5T620-640q-25 0-42.5 17.5T560-580q0 25 17.5 42.5T620-520Zm-280 0q25 0 42.5-17.5T400-580q0-25-17.5-42.5T340-640q-25 0-42.5 17.5T280-580q0 25 17.5 42.5T340-520Zm140 260q68 0 123.5-38.5T684-400h-66q-22 37-58.5 58.5T480-320q-43 0-79.5-21.5T342-400h-66q25 63 80.5 101.5T480-260Zm0 180q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg> },
     { id: 'text', title: '텍스트', icon: <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M429.33-166.67V-692H206.67v-101.83h546.66V-692H530.67v525.33H429.33Z"/></svg> },
   ];
+  
+  useEffect(() => {
+    console.log("subMenu :", subMenu);
+  }, [subMenu]);
 
   function handleMenuBar(id){
     setSubMenu(id);
@@ -21,23 +25,21 @@ export default function EditMenu(){
     setSubMenu(null);
   }
   
-  useEffect(() => {
-    console.log("subMenu :", subMenu);
-  }, [subMenu]);
-
   return (
     <div className={classes.editmenu_box}>
     {subMenu ? (
       <EditSubMenu menu={subMenu} handleBack={handleBack} />
     ):(
-      menuList.map((item, index) => (
-        <div key={index}>
-          <div className={classes.menu_box} onClick={() => handleMenuBar(item.id)}>
-            {item.icon}
-            <p className={classes.title}>{item.title}</p>
+      <div className={classes.menubar_box}>
+        {menuList.map((item, index) => (
+          <div key={index}>
+            <div className={classes.menu_box} onClick={() => handleMenuBar(item.id)}>
+              {item.icon}
+              <p className={classes.title}>{item.title}</p>
+            </div>
           </div>
-        </div>
-      ))
+        ))}
+      </div>
     )}
     </div>
 
--- components/EditSubMenu.jsx
+++ components/EditSubMenu.jsx
@@ -55,7 +55,7 @@ export default function EditSubMenu({ menu, handleBack }){
   }
 
   return (
-    <>
+    <div className={classes.submenu_box}>
       <div className={classes.back_btn} onClick={handleBack}>
         <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>
       </div>
@@ -65,6 +65,6 @@ export default function EditSubMenu({ menu, handleBack }){
           <p className={classes.title}>{item.title}</p>
         </div>
       ))}
-    </>
+    </div>
   );
 }
